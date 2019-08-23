51d704dade24d343e44aef4eb3e947765fea9e7c
Rui Lan <rlan@dgx17.aselab.nvidia.com>
2019-08-23 08:27:57 -0700

    Add code for nvcomp

--- xgb_debug.py
+++ xgb_debug.py
@@ -33,15 +33,15 @@ def simulate_data(m, n, k=2, random_state=None, classification=True):
                                             random_state=random_state, shuffle=True)
     return features.astype(np.float32), labels.astype(np.float32)
 
-model_path = "./"
-data_path = "./"
-result_path = "./"
+model_path = "./models/"
+data_path = "./data/"
+# result_path = "./"
 
 # control the test cases 
 n_trees = 100
-max_depth = 10
+max_depth = 5
 dataset = "higgs"
-dataset_row = 1000
+dataset_row = 11000000
 n_cols = 28
 
 save_load = False 
@@ -58,7 +58,7 @@ X_train = data.X_train.to_numpy(np.float32)
 X_test = data.X_test.to_numpy(np.float32)
 y_train = data.y_train.to_numpy(np.float32)
 y_test = data.y_test.to_numpy(np.float32)
-
+"""
 print("===>Training XGB - D: %d, T: %d, C: %d" % (max_depth, n_trees, n_cols))
 
 dtrain = xgb.DMatrix(X_train, label=y_train, silent=False)
@@ -89,13 +89,16 @@ print("    XGboost training time: ", stop_xgb - start_xgb)
 
 if save_load: 
     xgb_tree.save_model(model_path+'xgb_D'+str(max_depth)+'_T'+str(n_trees)+'_C'+str(n_cols)+'.model')
-    xgb_tree = xgb.Booster()
-    xgb_tree.load_model(model_path+'xgb_D'+str(max_depth)+'_T'+str(n_trees)+'_C'+str(n_cols)+'.model') 
+"""
 
-X_test_g = cuda.to_device(np.ascontiguousarray(X_test))
-X_test_c = X_test
-y_test_t = y_test
+xgb_tree = xgb.Booster()
+xgb_tree.load_model(model_path+'xgb_D'+str(max_depth)+'_T'+str(n_trees)+'_C'+str(n_cols)+'.model') 
 
+X_test_g = cuda.to_device(np.ascontiguousarray(X_test[:1000, :]))
+X_test_c = X_test[:1000, :]
+y_test_t = y_test[:1000]
+
+"""
 xgb_tree.set_param({'predictor': 'cpu_predictor'})
 # xgb_tree.set_param({'n_gpus': '0'})
 dtest = xgb.DMatrix(X_test_c, silent=False)
@@ -108,19 +111,36 @@ stop_xgb = time.time()
 xgb_acc_cpu = accuracy_score(xgb_preds_cpu > 0.5, y_test_t)
 
 print("    XGboost CPU testing time: ", (stop_xgb - start_xgb)*1000, " XGboost CPU acc: ", xgb_acc_cpu)
+"""
 
 xgb_tree.set_param({'predictor': 'gpu_predictor'})
 xgb_tree.set_param({'n_gpus': '1'})
 xgb_tree.set_param({'gpu_id': '0'})
-dtest = xgb.DMatrix(X_test_g, silent=False)
+dtest = xgb.DMatrix(X_test_c, silent=False)
 
-_ = xgb_tree.predict(dtest)
+each_run = []
+for i in range(repeat):
+    _ = xgb_tree.predict(dtest)
+    start_xgb = time.time()
+    xgb_preds_gpu = xgb_tree.predict(dtest)
+    stop_xgb = time.time()
+    each_run.append(stop_xgb - start_xgb)
+
+print(each_run)
+"""
+print("    XGboost GPU testing time: ", (stop_xgb - start_xgb)*1000)
 start_xgb = time.time()
 xgb_preds_gpu = xgb_tree.predict(dtest)
 stop_xgb = time.time()
+print("    XGboost GPU testing time: ", (stop_xgb - start_xgb)*1000)
+start_xgb = time.time()
+xgb_preds_gpu = xgb_tree.predict(dtest)
+stop_xgb = time.time()
+print("    XGboost GPU testing time: ", (stop_xgb - start_xgb)*1000)
+"""
 
 xgb_acc_gpu = accuracy_score(xgb_preds_gpu > 0.5, y_test_t)
 
-print("    XGboost GPU testing time: ", (stop_xgb - start_xgb)*1000, " XGboost GPU acc: ", xgb_acc_gpu)
+print("    XGboost GPU testing time: ", min(each_run)*1000, " XGboost GPU acc: ", xgb_acc_gpu)
 
 
